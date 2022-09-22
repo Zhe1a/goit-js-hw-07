e7b4f7c32d3e50bdde1e00cecb85707864da32bd
Zhe1a <mgr58006@gmail.com>
2022-09-22 22:22:45 +0300

    +++

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,21 +2,13 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const div = document.querySelector(".gallery");
 const img = gallery(galleryItems)
+div.addEventListener("click",galleryClick)
 
 
-div.insertAdjacentHTML("beforeend",img)
-
-document.querySelector('.gallery__item').onclick = () => {
-
-	basicLightbox.create(
-  gallery
-  ).show()
-
-}
 function gallery(params) {
     return params.map(({preview, description, original})=>
     `<div class="gallery__item">
-    <a class="gallery__link" href="large-image.jpg">
+    <a class="gallery__link" href="#">
       <img
         class="gallery__image"
         src="${preview}"
@@ -25,4 +17,26 @@ function gallery(params) {
       />
     </a>
   </div>`
-  ).join(" ")}
+  ).join(" ")};
+
+let rev = 0
+
+
+function galleryClick(evt){
+
+const isGalleryClick = evt.target.classList.contains("gallery__image");
+if(!isGalleryClick){
+  return
+}
+const data = evt.target.dataset.source;
+console.log(data);
+const instance = basicLightbox.create(
+
+  `
+    <img src="${data}" width="800" height="600">
+`)
+instance.show()
+}
+
+
+div.insertAdjacentHTML("beforeend",img)
