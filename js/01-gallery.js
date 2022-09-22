585ef516e61891843cd1f1d0fa5873cd89ba57b5
Zhe1a <mgr58006@gmail.com>
2022-09-22 18:14:23 +0300

    ++

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,16 +2,25 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const div = document.querySelector(".gallery");
 const img = gallery(galleryItems)
+
+
 div.insertAdjacentHTML("beforeend",img)
 
+document.querySelector('.gallery__item').onclick = () => {
+
+	basicLightbox.create(
+  gallery
+  ).show()
+
+}
 function gallery(params) {
-    return params.map(({preview,description})=>
+    return params.map(({preview, description, original})=>
     `<div class="gallery__item">
     <a class="gallery__link" href="large-image.jpg">
       <img
         class="gallery__image"
         src="${preview}"
-        data-source="large-image.jpg"
+        data-source="${original}"
         alt="${description}"
       />
     </a>
